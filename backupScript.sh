5c7003c2b3a20de4dbf22ab88ebb9fef1f2c6139
Yago <54825415+yagoSP@users.noreply.github.com>
2023-04-10 21:57:46 -0300

    Create backupScript.sh

---
+++ backupScript.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "BACKUO INICIADO"
+
+#COLETA DE DATA E HORA
+DATAHORA=`date +%b-%d-%y`
+
+#ORIGEM DOS ARQUIVOS
+ORIGEM=/home/chorelis/Desktop/origem
+
+#DESTINO 
+DESTINO=/home/chorelis/Desktop/destino/$DATAHORA-Backup.tar.gz
+
+#COMPACTANDO ARQUIVOS
+tar -cpzf $DESTINO $ORIGEM
+
+echo "BACKUP FINALIZADO!"
+
+#YagoSP 21:54 [10-04-2023] (SENAC / sala 18 / Michael)  
